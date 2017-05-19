a2e56d530e37ae2a4bea26df015669c52eecc685
Diego Lapiduz <dlapiduz@users.noreply.github.com>
2017-05-19 12:06:56 -0500

    Add install-node-exporter task

---
+++ pipeline/tasks/install-node-exporter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [[ -s om-bosh-creds/bosh-ca.pem ]]; then
+  bosh -n --ca-cert om-bosh-creds/bosh-ca.pem target `cat om-bosh-creds/director_ip`
+else
+  bosh -n target `cat om-bosh-creds/director_ip`
+fi
+
+BOSH_USERNAME=$(cat om-bosh-creds/bosh-username)
+BOSH_PASSWORD=$(cat om-bosh-creds/bosh-pass)
+
+echo "Logging in to BOSH..."
+bosh login <<EOF 1>/dev/null
+$BOSH_USERNAME
+$BOSH_PASSWORD
+EOF
+
+echo "Uploading Runtime Config..."
+bosh update runtime-config pcf-prometheus-git/runtime.yml
