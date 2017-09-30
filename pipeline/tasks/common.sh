54eee5d2efffff3775614fe718ed50fcf11aed58
Michal Kuratczyk <mkuratczyk@pivotal.io>
2017-10-01 00:14:20 +0100

    WIP: change image

--- pipeline/tasks/common.sh
+++ pipeline/tasks/common.sh
@@ -4,7 +4,7 @@ function login_to_director() {
   CREDS=$1
 
   export BOSH_CA_CERT=$(cat $CREDS/bosh-ca.pem)
-  export BOSH_ENVIRONMENT=$(cat $CREDS/director-ip)
+  export BOSH_ENVIRONMENT=$(cat $CREDS/director_ip)
   export BOSH_CLIENT=$(cat $CREDS/bosh-username)
   export BOSH_CLIENT_SECRET=$(cat $CREDS/bosh-pass)
 }
