1a2b6e1fe61282559cc9ee5eae8cce3e76f699e4
Michal Kuratczyk <mkuratczyk@pivotal.io>
2017-10-01 15:03:59 +0100

    WIP: fix variable name

--- pipeline/tasks/install-node-exporter.sh
+++ pipeline/tasks/install-node-exporter.sh
@@ -2,7 +2,7 @@
 set -e
 
 TMPDIR=${TMPDIR:-/tmp}
-runtime-config-file=$(mktemp "$TMPDIR/runtime-config.XXXXXX")
+TMPFILE=$(mktemp "$TMPDIR/runtime-config.XXXXXX")
 
 root_dir=$(cd "$(dirname "$0")/.." && pwd)
 
@@ -13,11 +13,11 @@ login_to_director pcf-bosh-creds
 # Check if existing runtime config is empty
 # Unfortunately bosh file.yml -o opsfile.yml fails if file.yml is empty
 # therefore we check if it is and apply runtime config appropriately
-bosh2 runtime-config > "${runtime-config-file}"
-lines=$(wc -l < "${runtime-config-file}" | xargs)
+bosh2 runtime-config > "${TMPFILE}"
+lines=$(wc -l < "${TMPFILE}" | xargs)
 echo "Uploading Runtime Config..."
 if [[ $lines -eq 1 ]]; then
     bosh2 update-runtime-config pcf-prometheus-git/runtime.yml
 else
-    bosh2 update-rntime-config "${runtime-config-file}" -o pcf-prometheus-git/runtime-ops.yml
+    bosh2 update-rntime-config "${TMPFILE}" -o pcf-prometheus-git/runtime-ops.yml
 fi
