f0856bbc4cc77edc4d680aef54bfd35ffff6ebd4
Michal Kuratczyk <mkuratczyk@pivotal.io>
2017-10-01 13:21:19 +0100

    WIP: fix authentication

--- pipeline/tasks/common.sh
+++ pipeline/tasks/common.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 function login_to_director() {
-  CREDS=$1
+	CREDS=${1}
 
-  export BOSH_CA_CERT=$(cat $CREDS/bosh-ca.pem)
-  export BOSH_ENVIRONMENT=$(cat $CREDS/director_ip)
-  export BOSH_CLIENT=$(cat $CREDS/bosh-username)
-  export BOSH_CLIENT_SECRET=$(cat $CREDS/bosh-pass)
+	if [[ -f $CREDS/bosh2_commandline_credentials ]]; then
+		source "$CREDS/bosh2_commandline_credentials"
+	else
+		export BOSH_CA_CERT=$(cat "$CREDS/bosh-ca.pem")
+		export BOSH_ENVIRONMENT="$(cat "$CREDS/director_ip")"
+		export BOSH_CLIENT=$(cat "$CREDS/bosh-username")
+		export BOSH_CLIENT_SECRET=$(cat "$CREDS/bosh-pass")
+	fi
 }
