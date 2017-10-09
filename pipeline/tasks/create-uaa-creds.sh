ff6b7dd8aa1da6ca8721e48d007d4793daa92c63
Michal Kuratczyk <mkuratczyk@pivotal.io>
2017-10-09 14:12:16 +0200

    client names are now hardcoded (as they are in prometheus-boshrelease)

--- pipeline/tasks/create-uaa-creds.sh
+++ pipeline/tasks/create-uaa-creds.sh
@@ -7,7 +7,7 @@ CURL="om --target https://${opsman_url} -k \
   curl"
 
 echo "Getting UAA credentials..."
-cf_id=$($CURL --path=/api/v0/deployed/products | jq -r ".[].guid" | grep cf-)
+cf_id=$($CURL --path=/api/v0/deployed/products | jq -r ".[].guid" | grep "^cf-")
 
 uaa_creds=$($CURL --path=/api/v0/deployed/products/$cf_id/credentials/.uaa.admin_client_credentials)
 
@@ -17,15 +17,15 @@ uaa_secret=$(echo $uaa_creds | jq -r .credential.value.password)
 echo "Creating Prometheus UAA Client..."
 uaac target https://uaa.${pcf_sys_domain} --skip-ssl-validation
 uaac token client get ${uaa_client} -s ${uaa_secret}
-uaac client add ${prometheus_firehose_client} \
-  --name ${prometheus_firehose_client} \
+uaac client add firehose_exporter \
+  --name firehose_exporter \
   --secret ${prometheus_firehose_secret} \
   --authorized_grant_types client_credentials,refresh_token \
   --authorities doppler.firehose || true #ignore errors
 
 echo "Creating Prometheus CF Client..."
-uaac client add ${prometheus_cf_client} \
-  --name ${prometheus_cf_client} \
+uaac client add cf_exporter \
+  --name cf_exporter \
   --secret ${prometheus_cf_secret} \
   --authorized_grant_types client_credentials,refresh_token \
   --authorities cloud_controller.admin_read_only || true
@@ -46,8 +46,8 @@ $uaa_admin_password
 EOF
 
 echo "Creating Prometheus BOSH UAA Client ..."
-uaac client add ${prometheus_bosh_client} \
-  --name ${prometheus_bosh_client} \
+uaac client add bosh_exporter \
+  --name bosh_exporter \
   --secret ${prometheus_bosh_secret} \
   --authorized_grant_types client_credentials,refresh_token \
   --authorities bosh.read \
