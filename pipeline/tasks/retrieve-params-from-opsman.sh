8fdbe6461b3453893aa9895ec267d018dcc7b860
Michal Kuratczyk <mkuratczyk@pivotal.io>
2017-10-18 00:36:49 +0200

    automatically discover doppler port

--- pipeline/tasks/retrieve-params-from-opsman.sh
+++ pipeline/tasks/retrieve-params-from-opsman.sh
@@ -12,3 +12,7 @@ cf_id=$($CURL --path=/api/v0/deployed/products | jq -r '.[] | select(.type == "c
 
 system_domain=$($CURL --path=/api/v0/deployed/products/$cf_id/manifest | jq -r '.instance_groups[] | select (.name == "cloud_controller") | .jobs[] | select (.name == "cloud_controller_ng") | .properties.system_domain')
 echo "system_domain: ${system_domain}" >> params/params.yml
+
+doppler_url=$($CURL --path=/api/v0/deployed/products/$cf_id/manifest | jq -r '.instance_groups[] | select(.name == "autoscaling") | .jobs[] | select (.name == "deploy-autoscaling") | .properties.doppler.host')
+traffic_controller_external_port=${doppler_url/*:/}
+echo "traffic_controller_external_port: ${traffic_controller_external_port}" >> params/params.yml
