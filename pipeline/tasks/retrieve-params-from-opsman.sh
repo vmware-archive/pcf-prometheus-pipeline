405497722fefe02223c64b4014ab6f2d6f468c00
Michal Kuratczyk <mkuratczyk@pivotal.io>
2017-10-18 00:30:51 +0200

    missing part

--- pipeline/tasks/retrieve-params-from-opsman.sh
+++ pipeline/tasks/retrieve-params-from-opsman.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-mkdir params
+mkdir params || true
+rm -f params/params.yml
+
+CURL="om --target https://${opsman_url} -k \
+  --username ${pcf_opsman_admin_username} \
+  --password ${pcf_opsman_admin_password} \
+  curl"
+
+cf_id=$($CURL --path=/api/v0/deployed/products | jq -r '.[] | select(.type == "cf") | .guid')
 
 system_domain=$($CURL --path=/api/v0/deployed/products/$cf_id/manifest | jq -r '.instance_groups[] | select (.name == "cloud_controller") | .jobs[] | select (.name == "cloud_controller_ng") | .properties.system_domain')
 echo "system_domain: ${system_domain}" >> params/params.yml
