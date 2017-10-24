42f8108281e913510eb38e4361d0ae19cefc3e54
Michal Kuratczyk <mkuratczyk@pivotal.io>
2017-10-24 14:38:34 +0200

    move files around

--- pipeline/tasks/retrieve-params-from-opsman.sh
+++ pipeline/tasks/retrieve-params-from-opsman.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-mkdir params || true
-rm -f params/params.yml
+mkdir pcf-prometheus-git/params || true
+PARAMS_FILE=pcf-prometheus-git/params/params.yml
+rm -f ${PARAMS_FILE}
 
 CURL="om --target https://${opsman_url} -k \
   --username ${pcf_opsman_admin_username} \
@@ -13,8 +14,11 @@ cf_id=$($CURL --path=/api/v0/deployed/products | jq -r '.[] | select(.type == "c
 $CURL --path=/api/v0/deployed/products/$cf_id/manifest > /tmp/cf-manifest.yml
 
 system_domain=$(jq -r '.instance_groups[] | select (.name == "cloud_controller") | .jobs[] | select (.name == "cloud_controller_ng") | .properties.system_domain' < /tmp/cf-manifest.yml)
-echo "system_domain: ${system_domain}" >> params/params.yml
+echo "system_domain: ${system_domain}" >> ${PARAMS_FILE}
 
 doppler_url=$(jq -r '.instance_groups[] | select(.name == "autoscaling") | .jobs[] | select (.name == "deploy-autoscaling") | .properties.doppler.host' < /tmp/cf-manifest.yml)
 traffic_controller_external_port=${doppler_url/*:/}
-echo "traffic_controller_external_port: ${traffic_controller_external_port}" >> params/params.yml
+echo "traffic_controller_external_port: ${traffic_controller_external_port}" >> ${PARAMS_FILE}
+
+metron_deployment_name=$(jq -r '.instance_groups[] | select(.name == "diego_brain") | .jobs[] | select(.name == "metron_agent") | .properties.metron_agent.deployment' < /tmp/cf-manifest.yml)
+echo "metron_deployment_name: ${metron_deployment_name}" >> ${PARAMS_FILE}
