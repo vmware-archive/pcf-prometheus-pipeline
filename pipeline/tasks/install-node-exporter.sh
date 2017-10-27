454ac9019c0adacba52716efd543964a945ba9f9
Michal Kuratczyk <mkuratczyk@pivotal.io>
2017-10-27 20:28:44 +0200

    update node_exporter to 2.0.0 and install it in a named runtime-config

--- pipeline/tasks/install-node-exporter.sh
+++ pipeline/tasks/install-node-exporter.sh
@@ -13,16 +13,4 @@ login_to_director pcf-bosh-creds
 echo "Uploading Node exporter Release..."
 bosh2 -n upload-release node-exporter-release/node-exporter-*.tgz
 
-# Check if existing runtime config is empty
-# Unfortunately bosh file.yml -o opsfile.yml fails if file.yml is empty
-# therefore we check if it is and apply runtime config appropriately
-bosh2 runtime-config > "${TMPFILE}"
-lines=$(wc -l < "${TMPFILE}" | xargs)
-echo "Uploading Runtime Config..."
-if [[ $lines -eq 1 ]]; then
-    bosh2 -n update-runtime-config pcf-prometheus-git/runtime.yml
-else
-    if ! grep -q "release: node-exporter" "${TMPFILE}"; then
-        bosh2 -n update-runtime-config "${TMPFILE}" -o pcf-prometheus-git/runtime-ops.yml
-    fi
-fi
+bosh2 -n update-runtime-config --name=node_exporter pcf-prometheus-git/runtime.yml
