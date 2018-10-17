105ea96d2edd97025852f97edb03506d51b9b836
Kazuto Kusama <jacopen@gmail.com>
2018-10-17 16:24:09 +0900

    Change bosh2 to bosh

--- pipeline/tasks/install-node-exporter.sh
+++ pipeline/tasks/install-node-exporter.sh
@@ -8,7 +8,7 @@ source ${root_dir}/tasks/common.sh
 login_to_director pcf-bosh-creds
 
 echo "Uploading Node exporter Release..."
-bosh2 -n upload-release node-exporter-release/node-exporter-*.tgz
+bosh -n upload-release node-exporter-release/node-exporter-*.tgz
 
 node_exporter_version=$(cat node-exporter-release/version)
-bosh2 -n update-runtime-config --name=node_exporter pcf-prometheus-pipeline/runtime.yml -v node_exporter_version=${node_exporter_version}
+bosh -n update-runtime-config --name=node_exporter pcf-prometheus-pipeline/runtime.yml -v node_exporter_version=${node_exporter_version}
